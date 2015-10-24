4c307e1219c1e642dcc471a587294c7cdd1df6c2
nikitasmall <nikitasosnov@yahoo.com>
2015-10-24 14:23:53 +0300

    implemented simple blocks for campaign info and social buttons

---
+++ src/blocks/campaign.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/*
+  Campaign Block
+*/
+
+var Block = require('../block');
+var stToHTML = require('../to-html');
+
+module.exports = Block.extend({
+
+  type: 'Campaign',
+
+  title: function(){ return 'Компания'; },
+
+  editorHTML: [
+    '<div class="st-block__inputs st-block__dropzone">',
+    '<span class="st-icon">poll</span>',
+    '<p class="twitter-tweet" align="center">Тут появится статистика об успехах компании.</p>',
+    '</div>'
+  ].join("\n"),
+
+  scribeOptions: {
+    allowBlockElements: false,
+    tags: {
+      p: false
+    }
+  },
+
+  icon_name: 'poll',
+
+  loadData: function(data){
+    if (this.options.convertFromMarkdown && data.format !== "html") {
+      this.setTextBlockHTML(stToHTML(data.text, this.type));
+    } else {
+      this.setTextBlockHTML(data.text);
+    }
+  }
+});
--- src/blocks/index.js
+++ src/blocks/index.js
@@ -4,6 +4,8 @@ module.exports = {
   Text: require('./text'),
   Quote: require('./quote'),
   Image: require('./image'),
+  Social: require('./social'),
+  Campaign: require('./campaign'),
   Heading: require('./heading'),
   List: require('./list'),
   Tweet: require('./tweet'),
